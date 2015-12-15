f84f397db1b4b685e17e70eb224d1fd37e353c9e
Dave Shuck <dshuck@gmail.com>
2015-12-14 23:49:46 -0600

    Update to include configuration targets in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,59 @@
 var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
-
+var merge = require('webpack-merge');
 var webpack = require('webpack');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
-  // Entry accepts a path or an object of entries.
-  // The build chapter contains an example of the latter.
+var common = {
   entry: PATHS.app,
-  output: {
+  // Given webpack-dev-server runs in-memory, we can drop
+  // `output`. We'll look into it again once we get to the
+  // build chapter.
+  /*output: {
     path: PATHS.build,
     filename: 'bundle.js'
-  },
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
-
-    // Display only errors to reduce the amount of output.
-    stats: 'errors-only',
-
-    // Parse host and port from env so this is easy to customize.
-    host: process.env.HOST,
-    port: process.env.PORT
+  },*/
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: PATHS.app
+      }
+    ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    // Important! move HotModuleReplacementPlugin below
+    //new webpack.HotModuleReplacementPlugin(),
     new HtmlwebpackPlugin({
       title: 'Kanban app'
     })
   ]
 };
+
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
